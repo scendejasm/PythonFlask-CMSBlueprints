6b5353d81718fe97d0f8d9b879f736538ff260c8
SCendejas <scendejasm@gmail.com>
2021-10-29 15:03:43 -0700

    Completed module2

--- cms/admin/__init__.py
+++ cms/admin/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, redirect, url_for, flash
 
-from cms.admin.models import Content, Type, Setting, User
+from cms.admin.models import Content, Type, Setting, User, db
 
-admin_bp = Blueprint('admin', __name__, url_prefix='/admin',template_folder='templates')
+app = Blueprint('admin', __name__, url_prefix='/admin',template_folder='templates')
 
 def requested_type(type):
     types = [row.name for row in Type.query.all()]
@@ -17,9 +17,30 @@ def content(type):
     else:
         abort(404)
 
-@admin_bp.route('/create/<type>')
+@admin_bp.route('/create/<type>', methods=('GET', 'POST'))
 def create(type):
     if requested_type(type):
+        if request.method == 'POST': 
+            title = request.form['title']
+            slug = request.form['slug']
+            type_id = request.form['type_id']
+            body = request.form['body']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+            elif not type_id:
+                error = 'Type is required.'
+
+            if error is None:
+                content = Content(title=title, slug=slug, type_id=type_id ,body=body)
+                db.session.add(content)
+                db.session.commit()
+                return redirect(url_for('admin.content', type=type))
+
+            flash(error)
+
+
         types = Type.query.all()
         return render_template('admin/content_form.html', title='Create', types=types, type_name=type)
     else:
